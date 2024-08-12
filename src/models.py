98bad81650191a6039d5a09e817df5f9af707f19
XimenaAvalos <164333573+XimenaAvalos@users.noreply.github.com>
2024-08-12 08:10:37 +0000

    API

--- src/models.py
+++ src/models.py
@@ -20,7 +20,7 @@ favorite_vehicle = db.Table('favorite_vehicle',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
-    last_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
